826083aa70267b0b12278818507fb939cf57065f
WIN-47TDCI7SIDM\SEHC <congdev95@gmail.com>
2023-07-28 17:05:19 +0700

    commit get all playlists and connection to mongodb

--- config/MongoConnector.py
+++ config/MongoConnector.py
@@ -1,8 +1,7 @@
 from urllib.parse import quote_plus
 
 from pymongo import MongoClient
-
-import utils
+from envyaml import EnvYAML
 
 
 class MongoManager:
@@ -18,12 +17,13 @@ class MongoManager:
         if MongoManager.__instance is not None:
             raise Exception("This class is a singleton!")
         # read file env.yaml and parse config
-        self.env = utils.getEnv()
+        self.env = EnvYAML("../config/config.yaml")
         self.mongodb_config = self.env['mongodb']
         self.host = self.mongodb_config['host']
         self.username = quote_plus(self.mongodb_config['username'])
         self.pwd = quote_plus(self.mongodb_config['password'])
         self.database = self.mongodb_config['database']
+        self.collections = self.mongodb_config["collections"]
         self.mongo = None
         self.client = None
         MongoManager.__instance = self
@@ -35,6 +35,17 @@ class MongoManager:
         # connection
         self.client = MongoClient(connection_string)
         self.mongo = self.client[self.database]
+
+    def is_connected(self):
+        if self.client and self.client.connected:
+            return True
+        return False
+    def insert_many_to_playplists(self, datas):
+        if not self.is_connected():
+            self.connect()
+        playlists = self.collections["playlists"]
+        playlists.insert_many(datas)
+
 
     def close_connection(self):
         if self.client is not None:
