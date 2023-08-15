460512b58b5415d3f35d0cff16d58a560f6219ee
WIN-47TDCI7SIDM\SEHC <congdev95@gmail.com>
2023-08-15 13:55:21 +0700

    add all file

--- scripts/playlits_processor.py
+++ scripts/playlits_processor.py
@@ -1,45 +1,25 @@
 import json
-
-class PlaylistProcessor:
-
-    def __init__(self):
-        self.playlists_file = '../data/craw/playlists_channel_data/all_playlist.json'
-        self.videos_file = '../data/craw/videos_data/{}'
+import os
 
 
-    def get_data(self, input):
-        # Lưu dữ liệu JSON vào thư mục data/raw với tên tệp là filename
-        with open(self.videos_file.format(input), "r", encoding="utf-8") as file:
-            data = json.load(file)
-            return data
+def get_id_playlists(uri_path):
+    target_titles = {
+        "Rap Việt": "[ LIVE STAGE ] | Rap Việt Mùa 3 (2023)",
+        "Người Ấy Là Ai?": "NGƯỜI ẤY LÀ AI? Mùa 5 (2023) | 20:00 Thứ 6 hàng tuần"
+    }
 
-    def process_videos_info(self, input):
-        video_ids = []
-        # Xử lý dữ liệu từ raw/video_data.json để lấy thông tin chi tiết về các video và trả về dữ liệu đã xử lý.
-        with open(self.videos_file.format(input), "r", encoding="utf-8") as file:
-            data = json.load(file)
-            for item in data:
-                video_ids.append(item["snippet"]["resourceId"]["videoId"])
-        return video_ids
+    id_mapping = {title: "" for title in target_titles.keys()}
+    with open(os.path.join(uri_path), "r", encoding="utf-8") as file:
+        data = json.load(file)
+        for item in data:
+            title = item["snippet"]["title"]
+            if title in target_titles.values():
+                for key, value in target_titles.items():
+                    if value == title and not id_mapping[key]:
+                        id_mapping[key] = item["id"]
+                        break
 
-    def process_playlist_info(self):
-        # Xử lý dữ liệu từ raw/video_data.json để lấy thông tin chi tiết về các video và trả về dữ liệu đã xử lý.
-        id_nala = ""
-        id_rapvie = ""
-        with open(self.playlists_file,  "r", encoding="utf-8") as file:
-            data = json.load(file)
-            for item in data:
-                if "[ LIVE STAGE ] | Rap Việt Mùa 3 (2023)" in item["snippet"]["title"]:
-                    id_rapvie = item["id"]
-                if "NGƯỜI ẤY LÀ AI? Mùa 5 (2023) | 20:00 Thứ 6 hàng tuần" in item["snippet"]["title"]:
-                    id_nala = item["id"]
-                if id_nala != "" and id_rapvie != "":
-                    return id_rapvie, id_nala
-        return id_rapvie, id_nala
+            if all(id_mapping.values()):
+                break
 
-
-
-    def process_analytics(self, analytics_data):
-        # Xử lý dữ liệu từ raw/analytics_data.json để lấy thông tin thống kê và trả về dữ liệu đã xử lý.
-        processed_analytics = [...]
-        return processed_analytics
+    return tuple(id_mapping.values())
