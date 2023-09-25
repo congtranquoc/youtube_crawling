356581ee3a3bf59d8ecaa530a9917a99de5d7776
WIN-47TDCI7SIDM\SEHC <congdev95@gmail.com>
2023-09-25 16:32:41 +0700

    add airflow

--- dags/youtube-dags.py
+++ dags/youtube-dags.py
@@ -3,24 +3,19 @@ from airflow import DAG
 from airflow.operators import bash_operator
 from airflow.utils.dates import days_ago
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.providers.google.cloud.operators.bigquery import BigQueryExecuteQueryOperator
-from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 
 # Define dag variables
 project_id = 'manifest-setup-397505'
-staging_dataset = 'newegg_dwh_staging'
-dwh_dataset = 'newegg_dwh_production'
-gs_bucket = 'de-airflow-bucket'
+staging_dataset = 'youtube_dwh_staging'
+dwh_dataset = 'youtube_dwh_production'
+gs_bucket = 'bigdata-python'
 
 # Define dag_args
 default_dag_args = {
-        "start_date":  days_ago(1)
-        ,"gcp_conn_id" : "gcp_connection_key"
-        , "retries": 3
-        , "retry_delay": timedelta(minutes=5)
-        , "email": "quoccong-workspace@gmail.com"
-        , "email_on_failure": True
-        , "email_on_retry": True
+        "start_date":  days_ago(1),
+        "gcp_conn_id" : "gcp_connection_key",
+        "retries": 3,
+        "retry_delay": timedelta(minutes=5)
 }
 
 # Define dag
@@ -39,51 +34,17 @@ crawl_data = bash_operator.BashOperator(
     dag=dag,
 )
 
-load_data_to_gcs = bash_operator.BashOperator(
+extract_data = bash_operator.BashOperator(
     task_id="extract_data",
-    bash_command="python3 /home/quoccong-workspace/airflow_project/scripts/shell/extract_data.sh ",
-    dag=dag,
-)
-# Load data from GCS to BQ
-load_data_to_staging_warehouse = GCSToBigQueryOperator(
-   task_id = 'load_data_to_staging_warehouse',
-   bucket = gs_bucket,
-   source_objects = ["newedge-graphic-cards.csv"],
-   destination_project_dataset_table = f'{project_id}:{staging_dataset}.newegg_data',
-   autodetect = True,
-   write_disposition='WRITE_TRUNCATE',
-   source_format = 'CSV',
-   encoding = "UTF-8",
-   field_delimiter=';',
-   skip_leading_rows = 1,
-   dag=dag,
-)
-
-# Check loaded data not null
-check_datas = BigQueryExecuteQueryOperator(
-    task_id = 'check_datas',
-    use_legacy_sql=False,
-    sql = f'SELECT count(*) FROM `{project_id}.{staging_dataset}.newegg_data`',
+    bash_command="bash /home/quoccong-workspace/airflow_project/scripts/shell/extract_data.sh ",
     dag=dag,
 )
 
-
-loaded_data_to_staging = DummyOperator(
-    task_id = 'loaded_data_to_staging',
-        dag=dag,
-)
-
-# Transform, load, and check fact data
-create_newegg_datas_on_production = BigQueryExecuteQueryOperator(
-    task_id = 'create_newegg_data',
-    use_legacy_sql = False,
-    params = {
-        'project_id': project_id,
-        'staging_dataset': staging_dataset,
-        'dwh_dataset': dwh_dataset
-    },
-    sql = './sql/newegg_dashboard_query.sql',
+migrate_data = bash_operator.BashOperator(
+    task_id="extract_data",
+    bash_command="bash /home/quoccong-workspace/airflow_project/scripts/shell/migrate_data.sh ",
     dag=dag,
 )
-start_pipeline >> extract_data >> load_data_to_gcs >> load_data_to_staging_warehouse >> check_datas >> loaded_data_to_staging >> create_newegg_datas_on_production
+
+start_pipeline >> crawl_data >> extract_data >> migrate_data
 
---
+++ scripts/shell/migrate_data.sh
@@ -0,0 +1,14 @@
+BUCKET="bigdata-python"
+FILE_NAMES = ("playlists_export.json" "videoids_export.json" "commentvideo_export.json" "statics_export.json")
+
+migrate_data(){
+  FILE_NAME=$1
+  URI_PATH = "/home/quoccong-workspace/airflow_project/data/${FILE_NAME}"
+  echo "Migrate data from uri '$URI_PATH' to gcs '$BUCKET'..."
+  gsutil -o "GSUtil:parallel_composite_upload_threshold=150M" -m cp "$URI_PATH" gs://"$BUCKET"
+}
+
+# Export data from MongoDB and process it for each collection
+for FILE_NAME in "${FILE_NAMES[@]}"; do
+    export_data "$FILE_NAME"
+done
